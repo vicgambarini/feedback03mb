12f87d69cad25e7d601938d1faefa4c3945f3e69
victoria.gambarini0 <victoria.gambarini0@gmail.com>
2025-03-31 02:24:17 +0000

    primeiro card da versão mobile finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,24 @@ import iconStar from "./assets/icon-star.svg"
 
 export function App() {
   return(
-    <div className="bg-gradient-dark text-white mx-6 p-6">
-      <div>
+    <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
+      <div className="bg-dark-blue w-fit p-4 rounded-full mb-4">
         <img src={iconStar} alt="icon star" />
       </div>
 
-      <h1>How did we do?</h1>
+      <h1 className="text-2xl font-bold mb-2.5 ">How did we do?</h1>
 
-      <p>Please let us!</p>
+      <p className="text-sm text-light-grey mb-6 leaing-1">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
 
-      <div>
-        <input type="button" value={1}/>
-        <input type="button" value={2}/>
-        <input type="button" value={3}/>
-        <input type="button" value={4}/>
-        <input type="button" value={5}/>
+      <div className="flex justify-between mb-6">
+        <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold mb-6"/>
+        <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold mb-6"/>
+        <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold mb-6"/>
+        <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold mb-6"/>
+        <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold mb-6"/>
       </div>
 
-      <button>Submit</button>
+      <button className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3">Submit</button>
     </div>
   )
 }
