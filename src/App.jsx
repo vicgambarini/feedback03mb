f3f8d780147a7cc2c54f2b499dbc191a687c08aa
victoria.gambarini0 <victoria.gambarini0@gmail.com>
2025-03-31 08:58:03 +0000

    criação de um estado para receber o valor da nota do usuário

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import iconStar from "./assets/icon-star.svg"
+import { useState } from "react"
 
 export function App() {
+  const [notaAvaliacao, setNotaAvaliacao] = useState(0)
+
+  function handleMudarNotaAvaliacao(nota) {
+    setNotaAvaliacao(bota)
+  }
+
   return(
     <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
       <div className="bg-dark-blue w-fit p-4 rounded-full mb-4">
@@ -12,14 +19,14 @@ export function App() {
       <p className="text-sm text-light-grey mb-6 leaing-1">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
 
       <div className="flex justify-between mb-6">
-        <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold mb-6"/>
-        <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold mb-6"/>
-        <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold mb-6"/>
-        <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold mb-6"/>
-        <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold mb-6"/>
+        <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold" onClick={() => handleMudarNotaAvaliacao(1)}/>
+        <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold" onClick={() => handleMudarNotaAvaliacao(2)}/>
+        <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold" onClick={() => handleMudarNotaAvaliacao(3)}/>
+        <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold" onClick={() => handleMudarNotaAvaliacao(4)}/>
+        <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold" onClick={() => handleMudarNotaAvaliacao(5)}/>
       </div>
 
       <button className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3">Submit</button>
     </div>
   )
-}
+} 
