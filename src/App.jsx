2acd00ac76acea3f05b0e4feb24d2b67b4d80c8a
victoria.gambarini0 <victoria.gambarini0@gmail.com>
2025-03-31 09:46:36 +0000

    adaptações de responsividade para o desktop e fim do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export function App() {
   const [submited, setSubmited] = useState(false)
 
   function handleMudarNotaAvaliacao(nota) {
-    setNotaAvaliacao(bota)
+    setNotaAvaliacao(nota)
   }
 
   function handleSubmit() {
@@ -22,7 +22,7 @@ export function App() {
 
   return(
     submited === false ? (
-    <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
+    <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass max-w-103">
       <div className="bg-dark-blue w-fit p-4 rounded-full mb-4">
         <img src={iconStar} alt="icon star" />
       </div>
@@ -32,17 +32,17 @@ export function App() {
       <p className="text-sm text-light-grey mb-6 leaing-1">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
 
       <div className="flex justify-between mb-6">
-        <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleMudarNotaAvaliacao(1)}/>
-        <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold  focus:bg-medium-grey focus:text-white" onClick={() => handleMudarNotaAvaliacao(2)}/>
-        <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold  focus:bg-medium-grey focus:text-white" onClick={() => handleMudarNotaAvaliacao(3)}/>
-        <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold  focus:bg-medium-grey focus:text-white" onClick={() => handleMudarNotaAvaliacao(4)}/>
-        <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleMudarNotaAvaliacao(5)}/>
+        <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13 hover:bg-orange hover:text-white hover: cursor-pointer" onClick={() => handleMudarNotaAvaliacao(1)}/>
+        <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold  focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13  hover:bg-orange hover:text-white hover: cursor-pointer" onClick={() => handleMudarNotaAvaliacao(2)}/>
+        <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold  focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13  hover:bg-orange hover:text-white hover: cursor-pointer" onClick={() => handleMudarNotaAvaliacao(3)}/>
+        <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold  focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13  hover:bg-orange hover:text-white hover: cursor-pointer" onClick={() => handleMudarNotaAvaliacao(4)}/>
+        <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13  hover:bg-orange hover:text-white hover: cursor-pointer" onClick={() => handleMudarNotaAvaliacao(5)}/>
       </div>
 
-      <button onClick={handleSubmit} className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3">Submit</button>
+      <button onClick={handleSubmit} className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3 hover:bg-white hover:text-orange hover: cursor-pointer">Submit</button>
     </div>
     ) : (
-      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass text-center">
+      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass max-w-103">
         <img className="mx-auto mb-6" src={ilustrationThankYou} alt="ilustration Thank You" />
 
         <p className="text-orange bg-dark-blue w-fit mx-auto px-3 py-1.5 rounded-3xl mb-6">You selected {notaAvaliacao} out of 5</p>
